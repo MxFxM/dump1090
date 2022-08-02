83a6fcd0fafd74427dd9f074c0d3b00f74ec9552
Max-Felix Müller <32713448+MxFxM@users.noreply.github.com>
2022-08-02 09:12:39 +0200

    dump

--- todatabase.py
+++ todatabase.py
@@ -4,6 +4,7 @@ import time
 import influxdb_client
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
+client = InfluxDBClient(url="http://mxfxm.com:8086", token="em3322x8rGjLjvjOVFwZDmA2FLqoUWytQrKjqfgpTaSuRSxdY4zkbqx5iNJwXx9ODf0-ywXQqzqQJFpfyxjjWQ==", org="mxfxm")
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
 # receive from stdin (piped input)
@@ -35,7 +36,7 @@ for line in sys.stdin:
         flight["messages"] = int(pieces[6])
         flight["seen"] = int(pieces[7])
         stringoutput = f"flightdata,id={flight['id']} altitude={flight['altitude']} speed={flight['speed']} lat={flight['lat']} lon={flight['lon']} track={flight['track']} messages={flight['messages']} seen={flight['seen']}"
-        p = influxdb_client.Point("flightdata").tag("device", "rtlsdr").field("id", flight["id"]).field("lat", flight["lat"]).field("lon", flight["lon"])
+        p = influxdb_client.Point("flightdata").tag("id", flight["id"]).field("lat", flight["lat"]).field("lon", flight["lon"]).field("altitude", flight["altitude"]).field("speed", flight["speed"]).field("track", flight["track"])
 
     elif len(pieces) == 10: #with flight number
         flight["id"] = pieces[0]
@@ -48,15 +49,17 @@ for line in sys.stdin:
         flight["messages"] = int(pieces[7])
         flight["seen"] = int(pieces[8])
         stringoutput = f"flightdata,id={flight['id']} flight={flight['flight']} altitude={flight['altitude']} speed={flight['speed']} lat={flight['lat']} lon={flight['lon']} track={flight['track']} messages={flight['messages']} seen={flight['seen']}"
-        p = influxdb_client.Point("flightdata").tag("device", "rtlsdr").field("id", flight["id"]).field("lat", flight["lat"]).field("lon", flight["lon"])
+        p = influxdb_client.Point("flightdata").tag("id", flight["id"]).tag("flight", flight["flight"]).field("lat", flight["lat"]).field("lon", flight["lon"]).field("altitude", flight["altitude"]).field("speed", flight["speed"]).field("track", flight["track"])
 
     else:
         # skip errors
         continue
 
     #print(f"{flight}")
-    print(stringoutput)
+    #print(stringoutput)
 
     #write_api.write("test_bucket", "mxfxm", [stringoutput])
-    write_api.write(bucket="test_bucket", org="mxfxm", record=p)
-    break
+    if flight["seen"] == 0:
+        # only write to database, if the data is really fresh
+        print(stringoutput)
+        write_api.write(bucket="live_bucket", org="mxfxm", record=p)
